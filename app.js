220ba20faef8db9d55225104bb79b5687d3df7e4
Kevin Eduardo <kevineduardoferreira@gmail.com>
2024-02-07 18:09:40 -0300

    change on the error message

--- app.js
+++ app.js
@@ -19,39 +19,40 @@ function checkCEP() {
 
     setTimeout(() => {
         fetch(`https://cep.awesomeapi.com.br/json/${cep}`)
-            .then(response => {
-                document.body.style.cursor = 'default';
-                if (!response.ok) {
-                    alert('CEP não encontrado!');
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (!data || data.erro) {
-                    result.innerHTML = '<p>CEP não encontrado!</p>';
-                    return;
-                }
-
-                result.innerHTML = 
-                `
-                <p>Endereço: ${data.address}</p>
-                <p>Bairro: ${data.district}</p>
-                <p>Cidade: ${data.city}</p>
-                <p>Estado: ${data.state}</p>
-                <p>Latitude: <span id="lat">${data.lat}</span></p>
-                <p>Longitude: <span id="lng">${data.lng}</span></p>
-                `;
-
-                result.innerHTML += '<button id="show-button">Exibir Mapa</button>';
-                
-                const showButton = document.getElementById("show-button");
-                showButton.addEventListener("click", showMap);
-            })
-            .catch(error => {
-                console.error('Erro ao tentar consultar o CEP:', error);
-                document.body.style.cursor = 'default';
-                result.innerHTML = '<p>Ocorreu um erro ao consultar o CEP!</p>';
-            });
+        .then(response => {
+            document.body.style.cursor = 'default';
+            if (!response.ok) {
+                result.innerHTML = '<p>Erro ao consultar o CEP!</p>';
+                return;
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (!data || data.erro) {
+                result.innerHTML = '<p>CEP não encontrado!</p>';
+                return;
+            }
+    
+            result.innerHTML = 
+            `
+            <p>Endereço: ${data.address}</p>
+            <p>Bairro: ${data.district}</p>
+            <p>Cidade: ${data.city}</p>
+            <p>Estado: ${data.state}</p>
+            <p>Latitude: <span id="lat">${data.lat}</span></p>
+            <p>Longitude: <span id="lng">${data.lng}</span></p>
+            `;
+    
+            result.innerHTML += '<button id="show-button">Exibir Mapa</button>';
+            
+            const showButton = document.getElementById("show-button");
+            showButton.addEventListener("click", showMap);
+        })
+        .catch(error => {
+            console.error('Erro ao tentar consultar o CEP:', error);
+            document.body.style.cursor = 'default';
+            result.innerHTML = '<p>Ocorreu um erro ao consultar o CEP!</p>';
+        });    
     }, 500);
 }
 
@@ -69,6 +70,7 @@ function showMap() {
     setTimeout(() => {
         let mapUrl = `https://maps.google.com/maps?q=${lat},${lng}&hl=pt&z=14&output=embed`;
         map.innerHTML = `<iframe src="${mapUrl}"></iframe>`;
+        document.getElementById("show-button").style.display = 'none';
         document.body.style.cursor = 'default';
     }, 500);
 }
