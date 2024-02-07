eebd20ba9c69c9fb8883c9f9b40f578603ac6366
Kevin Eduardo <kevineduardoferreira@gmail.com>
2024-02-07 17:56:06 -0300

    add settimeout at function showmap and request zip code, for show style wait of cursor

--- app.js
+++ app.js
@@ -17,40 +17,42 @@ function checkCEP() {
 
     document.body.style.cursor = 'wait';
 
-    fetch(`https://cep.awesomeapi.com.br/json/${cep}`)
-        .then(response => {
-            document.body.style.cursor = 'default';
-            if (!response.ok) {
-                alert('CEP não encontrado!');
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (!data || data.erro) {
-                result.innerHTML = '<p>CEP não encontrado!</p>';
-                return;
-            }
+    setTimeout(() => {
+        fetch(`https://cep.awesomeapi.com.br/json/${cep}`)
+            .then(response => {
+                document.body.style.cursor = 'default';
+                if (!response.ok) {
+                    alert('CEP não encontrado!');
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (!data || data.erro) {
+                    result.innerHTML = '<p>CEP não encontrado!</p>';
+                    return;
+                }
 
-            result.innerHTML = 
-            `
-            <p>Endereço: ${data.address}</p>
-            <p>Bairro: ${data.district}</p>
-            <p>Cidade: ${data.city}</p>
-            <p>Estado: ${data.state}</p>
-            <p>Latitude: <span id="lat">${data.lat}</span></p>
-            <p>Longitude: <span id="lng">${data.lng}</span></p>
-            `;
+                result.innerHTML = 
+                `
+                <p>Endereço: ${data.address}</p>
+                <p>Bairro: ${data.district}</p>
+                <p>Cidade: ${data.city}</p>
+                <p>Estado: ${data.state}</p>
+                <p>Latitude: <span id="lat">${data.lat}</span></p>
+                <p>Longitude: <span id="lng">${data.lng}</span></p>
+                `;
 
-            result.innerHTML += '<button id="show-button">Exibir Mapa</button>';
-            
-            const showButton = document.getElementById("show-button");
-            showButton.addEventListener("click", showMap);
-        })
-        .catch(error => {
-            console.error('Erro ao tentar consultar o CEP:', error);
-            document.body.style.cursor = 'default';
-            result.innerHTML = '<p>Ocorreu um erro ao consultar o CEP!</p>';
-        });
+                result.innerHTML += '<button id="show-button">Exibir Mapa</button>';
+                
+                const showButton = document.getElementById("show-button");
+                showButton.addEventListener("click", showMap);
+            })
+            .catch(error => {
+                console.error('Erro ao tentar consultar o CEP:', error);
+                document.body.style.cursor = 'default';
+                result.innerHTML = '<p>Ocorreu um erro ao consultar o CEP!</p>';
+            });
+    }, 500);
 }
 
 function showMap() {
@@ -62,7 +64,12 @@ function showMap() {
         return;
     }
 
-    let mapUrl = `https://maps.google.com/maps?q=${lat},${lng}&hl=pt&z=14&output=embed`;
+    document.body.style.cursor = 'wait';
 
-    map.innerHTML = `<iframe src="${mapUrl}"></iframe>`;
+    setTimeout(() => {
+        let mapUrl = `https://maps.google.com/maps?q=${lat},${lng}&hl=pt&z=14&output=embed`;
+        map.innerHTML = `<iframe src="${mapUrl}"></iframe>`;
+        document.body.style.cursor = 'default';
+    }, 500);
 }
+
